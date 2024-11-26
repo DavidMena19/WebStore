f447ff657cdcdd74b4637b0a31ff33d014e7135d
DavidMena18 <davidmena2500@gmail.com>
2024-11-26 19:01:46 -0400

    errorViewModel

---
+++ WebStore.Models/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    internal class ErrorViewModel
+    {
+        public string? RequestedId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestedId);
+    }
+}
